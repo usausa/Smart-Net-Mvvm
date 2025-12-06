a346d9d887390ad83b59c13310a4e73089980184
usausa <machi.pon@gmail.com>
2025-12-06 12:27:11 +0900

    Update generator

--- Smart.Mvvm.Generator/Diagnostics.cs
+++ Smart.Mvvm.Generator/Diagnostics.cs
@@ -5,7 +5,7 @@ using Microsoft.CodeAnalysis;
 internal static class Diagnostics
 {
     public static DiagnosticDescriptor InvalidPropertyDefinition => new(
-        id: "BTOP0001",
+        id: "SMV0001",
         title: "Invalid property definition",
         messageFormat: "Property must be partial. property=[{0}]",
         category: "Usage",
@@ -13,7 +13,7 @@ internal static class Diagnostics
         isEnabledByDefault: true);
 
     public static DiagnosticDescriptor PropertySetterRequired => new(
-        id: "BTOP0002",
+        id: "SMV0002",
         title: "Property setter is required",
         messageFormat: "Property setter is required. property=[{0}]",
         category: "Usage",
@@ -21,7 +21,7 @@ internal static class Diagnostics
         isEnabledByDefault: true);
 
     public static DiagnosticDescriptor InvalidTypeDefinition => new(
-        id: "BTOP0003",
+        id: "SMV0003",
         title: "Invalid type definition",
         messageFormat: "Type must extend ObservableObject. type=[{0}]",
         category: "Usage",
