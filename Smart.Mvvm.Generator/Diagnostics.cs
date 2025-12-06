8816f75a591f061aa8774a0ce10d99c076298b88
usausa <machi.pon@gmail.com>
2025-12-06 12:28:14 +0900

    Update generator

--- Smart.Mvvm.Generator/Diagnostics.cs
+++ Smart.Mvvm.Generator/Diagnostics.cs
@@ -4,7 +4,7 @@ using Microsoft.CodeAnalysis;
 
 internal static class Diagnostics
 {
-    public static DiagnosticDescriptor InvalidPropertyDefinition => new(
+    public static DiagnosticDescriptor InvalidPropertyDefinition { get; } = new(
         id: "SMV0001",
         title: "Invalid property definition",
         messageFormat: "Property must be partial. property=[{0}]",
@@ -12,7 +12,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor PropertySetterRequired => new(
+    public static DiagnosticDescriptor PropertySetterRequired { get; } = new(
         id: "SMV0002",
         title: "Property setter is required",
         messageFormat: "Property setter is required. property=[{0}]",
@@ -20,7 +20,7 @@ internal static class Diagnostics
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
 
-    public static DiagnosticDescriptor InvalidTypeDefinition => new(
+    public static DiagnosticDescriptor InvalidTypeDefinition { get; } = new(
         id: "SMV0003",
         title: "Invalid type definition",
         messageFormat: "Type must extend ObservableObject. type=[{0}]",
