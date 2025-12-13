3be4dd165631e3f4adf8e50e767a3b60afa56962
usausa <machi.pon@gmail.com>
2025-12-13 20:40:20 +0900

    Update view model base

--- Smart.Mvvm/Mvvm/ViewModels/ViewModelBase.cs
+++ Smart.Mvvm/Mvvm/ViewModels/ViewModelBase.cs
@@ -11,11 +11,15 @@ public abstract class ViewModelBase : ObservableObject, IDisposable
 
     private static readonly ViewModelOptions DefaultOptions = new();
 
+    private int disposed;
+
     private Disposables? disposables;
 
     // ------------------------------------------------------------
-    // Disposables
+    // Dispose
     // ------------------------------------------------------------
+
+    public bool IsDisposed => Interlocked.CompareExchange(ref disposed, 0, 0) == 1;
 
     protected Disposables Disposables => disposables ??= new Disposables();
 
@@ -49,26 +53,39 @@ public abstract class ViewModelBase : ObservableObject, IDisposable
         Errors.Handler = OnErrorChanged;
     }
 
+#pragma warning disable CA1063
     ~ViewModelBase()
     {
-        Dispose(false);
+        if (Interlocked.CompareExchange(ref disposed, 1, 0) == 0)
+        {
+            Dispose(false);
+        }
     }
+#pragma warning restore CA1063
 
+#pragma warning disable CA1063
     public void Dispose()
     {
-        Dispose(true);
-        GC.SuppressFinalize(this);
+        if (Interlocked.CompareExchange(ref disposed, 1, 0) == 0)
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
+#pragma warning restore CA1063
 
     protected virtual void Dispose(bool disposing)
     {
         if (disposing)
         {
+            // Release managed resources
             disposables?.Dispose();
             disposables = null;
 
             Errors.Dispose();
         }
+
+        // Release unmanaged resources
     }
 
     private void OnErrorChanged()
