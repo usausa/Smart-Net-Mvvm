a91c8f28f0ca5367a3dca09cea9d633f5e906e60
usausa <machi.pon@gmail.com>
2026-08-10 13:14:11 +0900

    Update

--- Smart.Mvvm.Generator/Models/PropertyModel.cs
+++ Smart.Mvvm.Generator/Models/PropertyModel.cs
@@ -5,16 +5,20 @@ using Microsoft.CodeAnalysis;
 using SourceGenerateHelper;
 
 internal sealed record PropertyModel(
+    // Containing type
     string Namespace,
     string TypeKey,
     EquatableArray<string> ContainingTypes,
     bool IsSealed,
+    // Options
     bool IsReactive,
     bool IsViewModel,
+    // Property signature
     Accessibility PropertyAccessibility,
     string PropertyType,
     string PropertyName,
     bool HasGetter,
     Accessibility? GetterAccessibility,
     Accessibility? SetterAccessibility,
+    // Notification targets
     EquatableArray<string> NotifyAlso);
